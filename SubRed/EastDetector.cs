fe981b3ac67210019d405e834083441fe90403ff
forfeellond <forfeellond@mail.ru>
2023-02-05 21:13:33 +0700

    EAST v1

---
+++ SubRed/EastDetector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using Emgu.CV.Dnn;
+using Emgu.CV.Util;
+
+namespace SubRed
+{
+    static class EastDetector
+    {
+        public static void EastDetect()
+        {
+            double confThreshold = 0.5;
+            double nmsThreshold = 0.4;
+            int inpWidth = 320;
+            int inpHeight = 320;
+            string model = "frozen_east_text_detector.pb";
+
+            Net net = DnnInvoke.ReadNet(model);
+            CvInvoke.NamedWindow("EAST");
+
+            VectorOfMat outs = new VectorOfMat();
+        }
+        
+    }
+}
