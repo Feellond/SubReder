6930a69f835ba2fa007a12efe95e2914683f46c6
forfeellond <forfeellond@mail.ru>
2023-02-28 17:29:50 +0700

    Интерфейс v2

---
+++ SubRed/SubtitleStyle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubRed
+{
+    class SubtitleStyle
+    {
+        public string Name { get; set; }
+        public string Fontname { get; set; }
+        public int Fontsize { get; set; }
+        public string PrimaryColor { get; set; }
+        public string SecondaryColor { get; set;}
+        public string OutlineColor { get; set; }
+        public bool Bold { get; set; }
+        public bool Italic { get; set; }
+        public bool Underline { get; set; }
+        public bool StrikeOut { get; set; }
+        public int ScaleX { get; set; }
+        public int ScaleY { get; set; }
+        public int SpacingX { get; set; }
+
+    }
+}
